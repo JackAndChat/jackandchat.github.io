9fd45b62c96d029c5954bbc8b4ce2edd22fda867
JackAndChat <107268019+JackAndChat@users.noreply.github.com>
2023-02-19 19:56:18 -0600

    Update common.js

--- beta/common.js
+++ beta/common.js
@@ -400,7 +400,7 @@ $(document).ready(function() {
       .appendTo("#leftcontrols")
       .on("click", function(){
         window.socket.emit("chatMsg", { msg: "/clear", meta: {} });
-        $(".chat-msg-Video").each(function(){ $(this).remove(); });
+        $messagebuffer.find("[class^=chat-msg-Video]").each(function(){ $(this).remove(); });
         socket.emit("playerReady");
       });
 
