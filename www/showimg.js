698af417409cd70b19d8dc01f5837d905ab7b1bb
JackAndChat <107268019+JackAndChat@users.noreply.github.com>
2023-02-20 16:29:26 -0600

    Update showimg.js

--- www/showimg.js
+++ www/showimg.js
@@ -73,10 +73,9 @@ const showChatImg = function() {
 
   $zoomImgMsg.find(imageExtensions).each(function() {
     let skip = false;
-    if ((this.href.indexOf("imgur.com") > -1) ||
-        (this.href.indexOf("redgifs.com") > -1)) {
-      skip = true;
-    }
+    if (((this.href.indexOf("redgifs.com") > -1) && (!this.href.indexOf("thumbs"))) ||
+        ((this.href.indexOf("imgur.com") > -1) && (!this.href.indexOf("i.imgur.com")))
+       ) { skip = true; }
 
     if (!skip) {
       var img = $('<img>',{class:'zoomImg',rel:'noopener noreferrer',title:'Click to Zoom',alt:'Bad Image'})
@@ -103,3 +102,41 @@ $(document).ready(function() {
   });
   showChatImg();
 });
+
+/*
+https://iframe.ly/api/iframely?api_key=d160d7c38aa4a3a3371b0c&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2F9somboon%2F35071348993%2Fin%2Fexplore-2017-07-12%2F
+
+https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hawtcelebs.com%2Fwp-content%2Fuploads%2F2020%2F02%2Femma-roberts-at-2020-vanity-fair-oscar-party-in-beverly-hills-02-09-2020-4.jpg&f=1&nofb=1&ipt=2cb299ec8c2e5bf2f1c530546ffabe8ff0c28208b6f8ae0f51351de3c8d76ffc&ipo=images
+
+const imageExtensions = 'a[href*=".jpg"], a[href*=".jpeg"], a[href*=".png"], a[href*=".pnj"], ' + 
+  'a[href*=".gif"], a[href*=".gifv"], a[href*=".svg"], a[href*=".svgz"], a[href*=".webm"], a[href*=".webp"]';
+
+$('#myModal').append($('<img>',{id:'img01',class:'modal-content',src:'theImg.png'}))
+$('<img>',{id:'img01',class:'modal-content',$img.attr('src')})
+
+<div id="myModal" class="modal">
+  <span class="imgClose">&times;</span>
+  <img class="modal-content" id="img01">
+</div>
+
+<a href="https://download.samplelib.com/mp4/sample-5s.mp4" target="_blank" rel="noopener noreferrer">
+  <video muted="" inline="" style="max-height: 72px; max-width: 160px;" title=" Click to Open in a Tab" src="https://download.samplelib.com/mp4/sample-5s.mp4" type="video/mp4"></video>
+  </a>
+
+<a href="http://docs.google.com/gview?url=https://sifr.in/img/292/1/courseAndroid.xlsx&embedded=true">Open your excel file</a>
+
+  $zoomImgMsg.find(videoExtensions).each(function() {
+    let thisParent = $(this).parent();
+    errorData("showChatImg.this", this.toString());
+    errorData("showChatImg.this", thisParent.html());
+    
+    var ext = 'mp4';
+    if (this.toString().toLowerCase().includes(".webm")) { ext = 'webm'; }
+    if (this.toString().toLowerCase().includes(".ogg"))  { ext = 'ogg'; }
+    
+    var video = `<a href="#" rel="noopener noreferrer" onClick='window.open("` + this.href + `");return false;'>` +
+      '<video muted inline style="max-height: 72px; max-width: 160px;" src="' + this.href + '" type="video/' + ext + '"  title=" Click to Open in a Tab" /></a>';
+    thisParent.html(video)
+  });
+
+*/
