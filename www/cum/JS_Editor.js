e4e85d4cec7cbe33ca65ca390c8a0d004ef3f5ad
JackAndChat <JackAndChat@gmail.com>
2023-03-20 10:56:28 -0500

    SoMuchCum

---
+++ www/cum/JS_Editor.js
@@ -0,0 +1,20 @@
+/*!
+**|  CyTube Customizations
+**|   
+**@preserve
+*/
+
+var ChannelName_Caption = 'So Much Cum!';
+var Room_ID = 'cum';
+
+var AGE_RESTRICT = true;
+var LOAD_BOT = true;
+
+var CLEAR_MSG = "Here is a list of other rooms https://s.lain.la/pQ2Z0 ";
+
+var MOTD_MSG = `<br /><span style="color:orange">Here is a list of other rooms <a style="color:orange;font-weight:600" target=_blank" href="https://s.lain.la/pQ2Z0">https://s.lain.la/pQ2Z0</a></span>`;
+
+// ##################################################################################################################################
+if (!window[CHANNEL.name]) { window[CHANNEL.name] = {}; }
+
+$.getScript("https://jackandchat.github.io/www/loader.js");
